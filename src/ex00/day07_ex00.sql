cf0b13f28ed721bf0550d22100c243df4cc85124
authentk <authentk@msk-school.21>
2026-03-12 18:55:40 +0300

    del

--- src/ex00/day07_ex00.sql
+++
@@ -1,4 +0,0 @@
-select pv.person_id, count(pv.id) as count_of_visits
-from person_visits pv join person p on p.id = pv.person_id 
-group by pv.person_id 
-order by count_of_visits desc, person_id asc;
